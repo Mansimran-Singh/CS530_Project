ce15cd4b7da0fa1e48678a41c4ef27a4accb3262
Daniel Andre Champagne <DAndreChampagne@gmail.com>
2021-09-25 09:54:42 -0400

    added runtime environment check

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const env = require('./env.js');
 const app = express();
 const path = require('path');
 
+
 // local modules
 const db = require('./db');
 const logger = require('./middleware/logger');
@@ -12,7 +13,7 @@ const logger = require('./middleware/logger');
 const port = env.httpPort ?? 5001;
 
 
-// additional modules
+// additional routes
 app.use('/notify', require('./notification'));
 app.use('/categories', require('./api/categories/categories'));
 
@@ -26,21 +27,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 // main application
-app.listen(port, () => {
-    console.log(`Example app listening at ${env.hostAddress}:${port}`);
-});
+
+if (env.runtime.node) {
+    app.listen(port, () => {
+        console.log(`Example app listening at ${env.hostAddress}:${port}`);
+    });
+}
 
 app.get('/', (req, res) => {
-
     db.client.connect((err, client) => {
         if (err !== undefined) {
-            res.sendStatus(401); // TODO: remove before prod
+            res.sendStatus(401);
         }
         res.sendFile(path.join(__dirname, 'views/api', 'index.html'))
-        // res.json(...);
-        // res.send(__dirname);
     });
-
 });
 
 app.get('/test/error', (req, res) => {
