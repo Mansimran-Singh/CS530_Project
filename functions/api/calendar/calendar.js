0cc79f5bfba2a8772153f1d9d152076d67167902
Daniel Andre Champagne <DAndreChampagne@gmail.com>
2021-10-22 18:09:12 -0400

    Update calendar.js

--- functions/api/calendar/calendar.js
+++ functions/api/calendar/calendar.js
@@ -158,157 +158,6 @@ async function insertEventAsync(oAuth2Client) {
 
 
 
-// /**
-//  * Create an OAuth2 client with the given credentials, and then execute the
-//  * given callback function.
-//  * @param {Object} credentials The authorization client credentials.
-//  * @param {function} callback The callback to call with the authorized client.
-//  */
-// function authorize(credentials, callback) {
-//   const {client_secret, client_id, redirect_uris} = credentials.installed;
-//   const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
-
-//   // Check if we have previously stored a token.
-//   fs.readFile(TOKEN_PATH, (err, token) => {
-//     if (err)
-//         return getAccessToken(oAuth2Client, callback);
-
-//     oAuth2Client.setCredentials(JSON.parse(token));
-
-//     return callback(oAuth2Client);
-//   });
-// }
-
-// /**
-//  * Get and store new token after prompting for user authorization, and then
-//  * execute the given callback with the authorized OAuth2 client.
-//  * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
-//  * @param {getEventsCallback} callback The callback for the authorized client.
-//  */
-// function getAccessToken(oAuth2Client, callback) {
-//   const authUrl = oAuth2Client.generateAuthUrl({
-//     access_type: 'offline',
-//     scope: SCOPES,
-//   });
-//   console.log('Authorize this app by visiting this url:', authUrl);
-  
-//   let code = '';
-
-//   oAuth2Client.getToken(code, (err, token) => {
-//     if (err)
-//         return console.error('Error retrieving access token', err);
-
-//     oAuth2Client.setCredentials(token);
-    
-//     // Store the token to disk for later program executions
-//     fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-//       if (err) 
-//         return console.error(err);
-//       console.log('Token stored to', TOKEN_PATH);
-//     });
-    
-//     return callback(oAuth2Client);
-//   });
-
-// }
-
-// /**
-//  * Lists the next 250 events on the user's primary calendar.
-//  * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
-//  */
-// function listEvents(auth) {
-//     const calendar = google.calendar({version: 'v3', auth});
-//     let params = {
-//         calendarId: env.googleCalendar.calendarId,
-//         // timeMin: (new Date()).toISOString(),
-//         maxResults: 250,
-//         singleEvents: true,
-//         orderBy: 'startTime',
-//     };
-
-//     console.info('authorize, after setCredentials');
-//     console.warn(JSON.stringify(auth, null, '\t'));
-
-//   calendar.events.list(params, (err, res) => {
-//     if (err) {
-//       console.log('The API returned an error: ' + err);
-//       return -1;
-//     }
-
-//     const events = res.data.items;
-
-//     let results = [];
-
-//     if (events.length) {
-//       console.log('Upcoming events:');
-//       events.map((event, i) => {
-//         const start = event.start.dateTime || event.start.date;
-//         const end = event.end.dateTime || event.end.date;
-//         console.log(`${start} - ${event.summary}`);
-//         results.push({
-//           id: event.id,
-//           summary: event.summary,
-//           description: event.description,
-//           startTime: start,
-//           endTime: end,
-//           htmlLink: event.htmlLink,
-//           event: event,
-//         });
-//       });
-//     } else {
-//       console.log('No upcoming events found.');
-//     }
-
-//     return results;
-//   });
-// }
-
-/**
- * Insert event into
- * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
- */
-// function insertEvent(auth) {
-//   const calendar = google.calendar({version: 'v3', auth});
-
-//   let tz = Intl.DateTimeFormat().resolvedOptions().timeZone;
-
-//   let start = moment().format();
-//   let end = moment(start).add(30, 'minutes').format();
-
-//   let params = {
-//     sendUpdates: 'all',
-//     colorId: null,
-//     start: {
-//       dateTime: start,
-//       timeZone: tz,
-//     },
-//     end: {
-//       dateTime: end,
-//       timeZone: tz,
-//     },
-//     summary: 'test',
-//     description: 'test event',
-//   };
-
-
-//   calendar.events.insert({calendarId: env.googleCalendar.calendarId, resource: params,}, (err, res) => {
-//     if (err) {
-//       return console.log('The API returned an error: ' + err);
-//     }
-
-//     console.log('created!');
-
-//   });
-// }
-
-
-
-
-
-
-
-
-
 async function _googleLoginAnd(func, resolve, reject, params) {
   authorizeAsync(env.googleCalendar.desktopCredentials)
     .then((value) => {
@@ -400,6 +249,11 @@ router.post('/token/set', (req, res) => {
   const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
 
   let code = req.body.token;
+  if (!code) {
+    res.status(400).send('No token provided in request body');
+    return;
+  }
+
 
   oAuth2Client.getToken(code, (err, token) => {
     if (err) {
@@ -428,4 +282,3 @@ router.post('/token/set', (req, res) => {
 
 
 module.exports = router;
-
