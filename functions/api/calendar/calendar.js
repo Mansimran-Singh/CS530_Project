cd7d35243a9a177e3eeffa28d4d38724505a9717
Maciej Bęcławski <m.beclawski@my.ccsu.edu>
2021-11-12 19:06:55 -0500

    cleanup

--- functions/api/calendar/calendar.js
+++ functions/api/calendar/calendar.js
@@ -30,44 +30,7 @@ router.get('/list', (req, res) => {
 	// 	"category": "Community"
 	// }
 router.post('/create', (req, res) => {
-	res.redirect(302, '/api/events' + req.path); return;
-
-	const tz = req.body.tz || Intl.DateTimeFormat().resolvedOptions().timeZone;
-	const start = moment().format();
-	const end = moment(start).add(30, 'minutes').format();
-
-	let params = {
-		sendUpdates: 'all',
-		colorId: req.body.colorId || null,
-		start: { dateTime: req.body.startTime || start, timeZone: tz, },
-		end: { dateTime: req.body.endTime || end, timeZone: tz, },
-		summary: req.body.summary || 'test',
-		description: req.body.description || 'test event',
-		category: req.body.category || 'Volunteer'
-	};
-
-
-	calendarApi.authorizeAsync(env.googleCalendar.getCredentials())
-		.then(
-			(value) => {
-				calendarApi.insertEventAsync(value, params)
-					.then(
-						(value) => { res.json(value); },
-						(reason) => { res.sendStatus(500); }
-					);
-			},
-			(reason) => {
-				calendarApi.getAccessTokenAsync(reason)
-					.then((value) => {
-						calendarApi.insertEventAsync(value, params)
-							.then(
-								(value) => { res.json(value); },
-								(reason) => { res.sendStatus(500); }
-							);
-					}, (reason) => {
-						res.status(500).send('Unable to get Google authentication token');
-					});
-		});
+	res.redirect(302, '/api/events' + req.path);
 });
 
 // https://developers.google.com/calendar/api/v3/reference/events/get
