e7a788d1b33be3133aa33b6427bb0ae195a2cbb6
Maciej Bęcławski <m.beclawski@my.ccsu.edu>
2021-11-22 16:58:24 -0500

    reverted previous commit, fixing on the front-end

--- functions/api/events/events.js
+++ functions/api/events/events.js
@@ -211,9 +211,6 @@ router.get('/:id', (req, res) => {
 						remoteResult.data.end.date = moment(remoteResult.data.end.date).subtract(1, "days").format("YYYY-MM-DD");
 					}
 
-					/** prevent error when nothing in mongo */
-					remoteResult.data.eventCategories = ['Uncat'];
-
 					db.client.connect().then( client => {
 						db.client.db(env.databaseName).collection('events').findOne({id: remoteResult.data.id})
 							.then(
